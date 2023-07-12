53657eb072d662df7ff38d0d858a029a6f8a3b4e
Марина <@MARINA>
2023-07-12 21:38:28 +0300

    ComboBox DONE

--- ComboBox_1/main.cpp
+++ ComboBox_1/main.cpp
@@ -1,33 +1,54 @@
+#define _CRT_SECURE_NO_WARNINGS
 #include<Windows.h>
+#include<cstdio>
 #include"resource.h"
+
+CONST CHAR* values[] = { "This","is","my","First","Combo","Box" };
 
 BOOL CALLBACK DlgProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam);
 
 INT WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInst, LPSTR lpCmdLine, INT nCmdShow)
 {
-	DialogBoxParam(hInstance, MAKEINTRESOURCE(IDD_DIALOG1), NULL, DlgProc, NULL);
+	//MessageBox(NULL, "ComboBox check", "Info", MB_YESNOCANCEL | MB_ICONINFORMATION);
+	DialogBoxParam(hInstance, MAKEINTRESOURCE(IDD_DIALOG1), NULL, (DLGPROC)DlgProc, NULL);
 	return 0;
-
 }
 
 BOOL CALLBACK DlgProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
 {
 	switch (uMsg)
 	{
-		case WM_INITDIALOG:
+	case WM_INITDIALOG:
+	{
+		HICON hIcon = LoadIcon(GetModuleHandle(NULL), MAKEINTRESOURCE(IDI_ICON1));
+		SendMessage(hwnd, WM_SETICON, 0, (LPARAM)hIcon);
+
+		HWND hCombo = GetDlgItem(hwnd, IDC_COMBO1);
+		for (int i = 0; i < sizeof(values) / sizeof(values[0]); i++)
 		{
-			HICON hIcon = LoadIcon(GetModuleHandle(NULL), MAKEINTRESOURCE(IDI_ICON1));
-			SendMessage(hwnd, WM_SETICON, 0, (LPARAM)hIcon);
+			SendMessage(hCombo, CB_ADDSTRING, 0, (LPARAM)values[i]);
+		}
+	}
+	break;
+	case WM_COMMAND:
+		switch (LOWORD(wParam))
+		{
+		case IDOK:
+		{
+			HWND hCombo = GetDlgItem(hwnd, IDC_COMBO1);
+			CONST INT SIZE = 256;
+			CHAR sz_buffer[SIZE]{};
+			int i = SendMessage(hCombo, CB_GETCURSEL, 0, 0); //определяем номер выбраного элемента выпадающего списка
+			SendMessage(hCombo, CB_GETLBTEXT, i, (LPARAM)sz_buffer);
+			CHAR sz_massege[SIZE] = {}; 
+			sprintf(sz_massege, "Вы выбрали элемент № %i, со значением \"%s\"", i, sz_buffer);
+			MessageBox(hwnd, sz_massege, "Info", MB_OK | MB_ICONINFORMATION);
 		}
 		break;
-		case WM_COMMAND:
-			switch (LOWORD(wParam))
-			{
-				case IDOK: EndDialog(hwnd, 0); break;
-				case IDCANCEL: EndDialog(hwnd, 0); break;
-			}
-			break;
-			case WM_CLOSE:EndDialog(hwnd, 0);
+		case IDCANCEL: EndDialog(hwnd, 0); break;
+		}
+		break;
+	case WM_CLOSE:EndDialog(hwnd, 0);
 	}
 	return FALSE;
 }
