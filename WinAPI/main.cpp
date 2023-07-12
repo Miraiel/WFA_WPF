e3440edcd761a512f4c07a1349d75af386a62b9a
Марина <@MARINA>
2023-07-12 20:14:19 +0300

    DONE

--- WinAPI/main.cpp
+++ WinAPI/main.cpp
@@ -1,6 +1,8 @@
 ﻿#include<Windows.h>
 #include"resource.h"
 
+CONST CHAR g_sz_LOGIN_INVITE[] = "Введите имя пользователя";
+
 BOOL CALLBACK DlgProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam);
 
 INT WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInst, LPSTR lpCmdLine, INT nCmdShow)
@@ -16,53 +18,59 @@ BOOL CALLBACK DlgProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
 {
 	switch (uMsg)
 	{
-		case WM_INITDIALOG: //Создаем элементы окна
+	case WM_INITDIALOG: //Создаем элементы окна
+	{
+		HICON hIcon = LoadIcon(GetModuleHandle(NULL), MAKEINTRESOURCE(IDI_ICON1));
+		SendMessage(hwnd, WM_SETICON, 0, (LPARAM)hIcon);	//иконка
+
+		HWND hdlgItemLogin;
+		hdlgItemLogin = GetDlgItem(hwnd, IDC_EDIT1_LOGIN);
+		SendMessage(hdlgItemLogin, WM_SETTEXT, 0, (LPARAM)g_sz_LOGIN_INVITE);
+
+		HWND hdlgItemPass;
+		hdlgItemPass = GetDlgItem(hwnd, IDC_EDIT2_PASSWORD);
+		SendMessage(hdlgItemPass, WM_SETTEXT, 0, (LPARAM)("Введите пароль"));
+
+	}
+	break;
+	case WM_COMMAND:	//Обрабатываем команды
+		switch (LOWORD(wParam))
 		{
-			HICON hIcon = LoadIcon(GetModuleHandle(NULL), MAKEINTRESOURCE(IDI_ICON1));
-			SendMessage(hwnd, WM_SETICON, 0, (LPARAM)hIcon);	//иконка
-
-			HWND hdlgItemLogin;
-			hdlgItemLogin = GetDlgItem(hwnd, IDC_EDIT1_LOGIN);
-			//SendMessage(hdlgItemLogin, WM_ENABLE, (WPARAM)(TRUE), 0);
-			SendMessage(hdlgItemLogin, WM_SETTEXT, 0, (LPARAM)("Введите имя пользователя"));
-
-			HWND hdlgItemPass;
-			hdlgItemPass = GetDlgItem(hwnd, IDC_EDIT2_PASSWORD);
-			//SendMessage(hdlgItemPass, EN_CHANGE, (WPARAM)(TRUE), 0);
-			SendMessage(hdlgItemPass, WM_SETTEXT, 0, (LPARAM)("Enter Password"));
-
-		}
-		break;
-		case WM_COMMAND:	//Обрабатываем команды
-			
-			switch (LOWORD(wParam))
+			case IDC_EDIT1_LOGIN:
 			{
-				case IDC_BUTTON1_COPY:
+				HWND hEditLogin = GetDlgItem(hwnd, IDC_EDIT1_LOGIN);
+				CONST INT SIZE = 256;
+				CHAR sz_buffer[SIZE] = {};
+				SendMessage(hEditLogin, WM_GETTEXT, SIZE, (LPARAM)sz_buffer);
+				if (HIWORD(wParam) == EN_SETFOCUS)
 				{
-					HWND hEditLogin = GetDlgItem(hwnd, IDC_EDIT1_LOGIN);
-					HWND hEditPassword = GetDlgItem(hwnd, IDC_EDIT2_PASSWORD);
-					const int SIZE = 256;
-					char sz_buffer[SIZE] = {};
-					SendMessage(hEditLogin, WM_GETTEXT, SIZE, (LPARAM)sz_buffer);
-					SendMessage(hEditPassword, WM_SETTEXT, 0, (LPARAM)sz_buffer);
-					SendMessage(hwnd, WM_SETTEXT, 0, (LPARAM)sz_buffer);
-					SendMessage(GetDlgItem(hwnd, IDC_STATIC_LOGIN), WM_SETTEXT, 0, (LPARAM)sz_buffer);
+					if (strcmp(sz_buffer, g_sz_LOGIN_INVITE) == 0)
+						SendMessage(hEditLogin, WM_SETTEXT, 0, (LPARAM)"");
 				}
-				break;
-				case IDOK: MessageBox(hwnd, "Была нажата кнопка ОК", "Info", MB_OK | MB_ICONINFORMATION); break;
-				case IDCANCEL: EndDialog(hwnd, 0); break;
-			}
-
-			switch (HIWORD(wParam))
-			{
-				case EN_KILLFOCUS:
+				if (HIWORD(wParam) == EN_KILLFOCUS)
 				{
-
+					if (strlen(sz_buffer) == 0)
+						SendMessage(hEditLogin, WM_SETTEXT, 0, (LPARAM)g_sz_LOGIN_INVITE);
 				}
-				break;
 			}
 			break;
-			case WM_CLOSE: EndDialog(hwnd, 0);
+		case IDC_BUTTON1_COPY:
+		{
+			HWND hEditLogin = GetDlgItem(hwnd, IDC_EDIT1_LOGIN);
+			HWND hEditPassword = GetDlgItem(hwnd, IDC_EDIT2_PASSWORD);
+			const int SIZE = 256;
+			char sz_buffer[SIZE] = {};
+			SendMessage(hEditLogin, WM_GETTEXT, SIZE, (LPARAM)sz_buffer);
+			SendMessage(hEditPassword, WM_SETTEXT, 0, (LPARAM)sz_buffer);
+			SendMessage(hwnd, WM_SETTEXT, 0, (LPARAM)sz_buffer);
+			SendMessage(GetDlgItem(hwnd, IDC_STATIC_LOGIN), WM_SETTEXT, 0, (LPARAM)sz_buffer);
+		}
+		break;
+		case IDOK: MessageBox(hwnd, "Была нажата кнопка ОК", "Info", MB_OK | MB_ICONINFORMATION); break;
+		case IDCANCEL: EndDialog(hwnd, 0); break;
+		}
+		break;
+	case WM_CLOSE: EndDialog(hwnd, 0);
 	}
 	return FALSE;
 }
