c2e92d38ba5c3f5db411ff01d9cfaf9fa7634e06
Марина <@MARINA>
2023-07-14 20:48:09 +0300

    ListBox_inProg

--- ListBox/main.cpp
+++ ListBox/main.cpp
@@ -3,15 +3,16 @@
 #include<cstdio>
 #include"resource.h"
 
-CONST CHAR* values[] = { "This","is","my","First","List","Box" };
+CONST CHAR* values[] = { "This","is","my","first","List","Box" };
 
 
 BOOL CALLBACK DlgProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam);
+BOOL CALLBACK DlgProcAdd(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lPAram);
 
 
 INT WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInst, LPSTR lpCmdLine, INT nCmdShow)
 {
-	DialogBoxParam(hInstance, MAKEINTRESOURCE(IDD_DIALOG1), NULL, DlgProc, NULL);
+	DialogBoxParam(hInstance, MAKEINTRESOURCE(IDD_DIALOG1), NULL, DlgProc, 0);
 	return 0;
 }
 
@@ -19,28 +20,76 @@ BOOL CALLBACK DlgProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
 {
 	switch (uMsg)
 	{
-		case WM_INITDIALOG:
+	case WM_INITDIALOG:
+	{
+		HWND hList = GetDlgItem(hwnd, IDC_LIST1);
+		for (int i = 0; i < sizeof(values) / sizeof(values[0]); i++)
 		{
-			
-			HWND hList = GetDlgItem(hwnd, IDC_LIST1);
+			SendMessage(hList, LB_ADDSTRING, 0, (LPARAM)values[i]);
+		}
 
-			for (int i = 0; i < ARRAYSIZE(values); i++)
-			{
-				SendMessage(hList, LB_ADDSTRING, 0, (LPARAM)values[i]);
-			}
-			//SetFocus(hList);
+	}break;
 
+	case WM_COMMAND:
+		switch (LOWORD(wParam))
+		{
+
+		case IDC_BUTTON1_ADD:
+		{
+			DialogBoxParam(GetModuleHandle(NULL), MAKEINTRESOURCE(IDD_DIALOG_ADD), hwnd, DlgProcAdd, 0);
 		}break;
 
-		case WM_COMMAND:
-			switch (LOWORD(wParam))
-			{
-				case IDOK:
-				{
-					
-				}break;
-			}
-		case WM_CLOSE: EndDialog(hwnd, 0);
+		case IDC_BUTTON_DELETE:
+
+			HWND hList = GetDlgItem(hwnd, IDC_LIST1);
+			SendMessage(hList, LB_GETTEXT, SendMessage(hList, LB_GETTEXT, 0, 0),0);
+			break;
+
+		case IDOK:
+		{
+			HWND hList = GetDlgItem(hwnd, IDC_LIST1);
+			int i = SendMessage(hList, LB_GETCURSEL, 0, 0);
+			CONST INT SIZE = 256;
+			CHAR sz_buffer[SIZE]{};
+			SendMessage(hList, LB_GETTEXT, i, (LPARAM)sz_buffer);
+			CHAR sz_massege[SIZE] = {};
+			sprintf(sz_massege, "Вы выбрали элемент № %i, со значением \"%s\"", i, sz_buffer);
+			MessageBox(hwnd, sz_massege, "Info", MB_OK | MB_ICONINFORMATION);
+		}break;
+
+
+		case IDCANCEL: EndDialog(hwnd, 0); break;
+		}break;
+	case WM_CLOSE:EndDialog(hwnd, 0);
+	}
+	return FALSE;
+}
+
+BOOL CALLBACK DlgProcAdd(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lPAram)
+{
+	switch (uMsg)
+	{
+	case WM_INITDIALOG:
+		break;
+
+	case WM_COMMAND:
+		switch (LOWORD(wParam))
+		{
+		case IDOK:
+			const INT SIZE = 256;
+			CHAR sz_buffer[SIZE] = {};
+			SendMessage(GetDlgItem(hwnd, IDC_EDIT_ADD), WM_GETTEXT, SIZE, (LPARAM)sz_buffer);
+
+			//получаем родительское окно
+			HWND hParent = GetParent(hwnd);
+			HWND hList = GetDlgItem(hParent, IDC_LIST1);
+			InSendMessage(hList, LB_ADDSTRING, 0, (LPARAM)sz_buffer);
+
+		}break;
+		case IDCANCEL:
+			break;
+
+	case WM_CLOSE: EndDialog(hwnd, 0);
 	}
 	return FALSE;
 }
