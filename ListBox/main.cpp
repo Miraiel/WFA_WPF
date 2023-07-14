8865eca4793ae107cafe524365f3037702c6de63
pokidyuk <pokidyuk@MR012>
2023-07-14 15:55:09 +0300

    ListBox_inProg

---
+++ ListBox/main.cpp
@@ -0,0 +1,52 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include<Windows.h>
+#include<cstdio>
+#include"resource.h"
+
+CONST CHAR* values[] = { TEXT("This"),"is","my","First","List","Box" };
+
+
+BOOL CALLBACK DlgProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam);
+
+
+INT WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInst, LPSTR lpCmdLine, INT nCmdShow)
+{
+	DialogBoxParam(hInstance, MAKEINTRESOURCE(IDD_DIALOG1), NULL, DlgProc, NULL);
+	return 0;
+}
+
+BOOL CALLBACK DlgProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
+{
+	switch (uMsg)
+	{
+		case WM_INITDIALOG:
+		{
+			HWND hList = GetDlgItem(hwnd, IDC_LIST1);
+
+			for (int i = 0; i < ARRAYSIZE(values); i++)
+			{
+				SendMessage(hList, LB_ADDSTRING, 0, (LPARAM)values[i]);
+			}
+			//SetFocus(hList);
+
+		}break;
+
+		case WM_COMMAND:
+			switch (LOWORD(wParam))
+			{
+
+				case IDC_LIST1:
+				{
+
+				}break;
+
+				case IDOK:
+				{
+					
+				}break;
+
+			}
+		case WM_CLOSE: EndDialog(hwnd, 0);
+	}
+	return FALSE;
+}
